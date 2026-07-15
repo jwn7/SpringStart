1d02c521ca0146b17b78bf821904db7e9d33e2b3
jwn7 <ksyzing@naver.com>
2026-07-15 17:50:18 +0900

    Add JPA study record entity

---
+++ src/main/java/com/springstart/study/persistence/StudyRecordEntity.java
@@ -0,0 +1,53 @@
+package com.springstart.study.persistence;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "study_records")
+public class StudyRecordEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "study_minutes")
+    private int studyMinutes;
+
+    private boolean completed;
+
+    protected StudyRecordEntity() {
+    }
+
+    public StudyRecordEntity(String title, String content, int studyMinutes) {
+        this.title = title;
+        this.content = content;
+        this.studyMinutes = studyMinutes;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getStudyMinutes() {
+        return studyMinutes;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
